3afe53eeef05dc66c3257a473450da09c38ef765
Maulik Dawda <dawdamaulik@gmail.com>
2026-07-01 15:37:32 +0530

    Move configuration to .env

--- app/Views/reports/print-tickets.php
+++ app/Views/reports/print-tickets.php
@@ -216,7 +216,7 @@
 
 
                 <img
-                    src="<?= BASE_URL ?>/public\assets\images\samtech-logo-report.png"
+                    src="<?= BASE_URL ?>/assets/images/samtech-logo-report.png"
                     class="logo"
                     alt="Samtech">
             </div>
