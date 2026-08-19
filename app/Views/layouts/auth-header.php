fdcffe58826b1b90deba2701aceeae4b9496a794
Maulik Dawda <dawdamaulik@gmail.com>
2026-08-19 16:47:04 +0530

    Minor update to trigger deployment sync on live server

--- app/Views/layouts/auth-header.php
+++ app/Views/layouts/auth-header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
 
+    <!-- Samtech Support Portal -->
     <title>Samtech Helpdesk</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
