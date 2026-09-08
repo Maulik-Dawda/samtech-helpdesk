b8be0d081484f71858a212d2bd2b5e27b98c3de4
Maulik Dawda <dawdamaulik@gmail.com>
2026-09-08 22:34:27 +0530

    Revert "Redesign system CSS theme, sidebar, topbar, dashboard metric cards, and ticket tables to match mockup UI design"
    This reverts commit 1caac8efd24885f646c1c79a8a263afa23dc2e02.

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -400,7 +400,7 @@ if ($isAdmin) {
 
     <div class="sidebar-footer">
 
-        <a href="<?= BASE_URL ?>/profile" class="sidebar-account">
+        <div class="sidebar-account">
 
             <div class="sidebar-account-avatar">
                 <?= htmlspecialchars($sidebarInitials); ?>
@@ -418,9 +418,17 @@ if ($isAdmin) {
 
             </div>
 
-            <i class="bi bi-chevron-right sidebar-account-arrow"></i>
+            <button
+                type="button"
+                class="sidebar-account-logout"
+                data-bs-toggle="modal"
+                data-bs-target="#logoutModal"
+                aria-label="Logout"
+            >
+                <i class="bi bi-box-arrow-right"></i>
+            </button>
 
-        </a>
+        </div>
 
     </div>
 
