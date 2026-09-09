6dbb7e10a779f5fa7cd56bb94a58bd2174082806
Maulik Dawda <dawdamaulik@gmail.com>
2026-09-09 17:17:19 +0530

    Fix favicon link path in auth pages layout to point to samtech-icon.png

--- app/Views/layouts/auth-header.php
+++ app/Views/layouts/auth-header.php
@@ -14,10 +14,9 @@
     <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/app.css?v=<?= time(); ?>">
     <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/forms.css?v=<?= time(); ?>">
 
-    <link rel="icon" type="image/png" href="<?= BASE_URL ?>/favicon.png">
-    <link rel="shortcut icon" type="image/png" href="<?= BASE_URL ?>/favicon.ico">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= BASE_URL ?>/apple-touch-icon.png">
-    <link rel="apple-touch-icon-precomposed" href="<?= BASE_URL ?>/apple-touch-icon-precomposed.png">
+    <link rel="icon" type="image/png" href="<?= BASE_URL ?>/assets/images/samtech-icon.png">
+    <link rel="shortcut icon" type="image/png" href="<?= BASE_URL ?>/assets/images/samtech-icon.png">
+    <link rel="apple-touch-icon" href="<?= BASE_URL ?>/assets/images/samtech-icon.png">
 </head>
 
 <body class="auth-body">
